7d8fcbd5d673337584ebc4f94304b5faf6f32cca
Gabriel <gbryan6@hotmail.com>
2020-12-22 22:04:02 -0300

    feat: finished backend

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,21 @@
 const express = require("express");
 
 const OngController = require("./controllers/OngController");
+const IncidentsController = require("./controllers/IncidentsController");
+const ProfileController = require("./controllers/ProfileController");
+const SessionController = require("./controllers/SessionController");
 
 const routes = express.Router();
 
-routes.get("/ongs", OngController.list);
+routes.post('/sessions', SessionController.create);
 
+routes.get('/profile', ProfileController.list);
+
+routes.get("/ongs", OngController.list);
 routes.post("/ongs", OngController.create);
+
+routes.post('/incidents', IncidentsController.create);
+routes.get('/incidents', IncidentsController.list);
+routes.delete('/incidents/:id', IncidentsController.delete);
 
 module.exports = routes;
